b11afe308a47b9e9a908b9bbd7f9d325f1af6ea1
Wender <wendermaiac@gmail.com>
2023-10-18 09:06:19 -0300

    exercicio finalizado

---
+++ Semana 1 - Introducao ao Javascript/ECMAScript/exercicioDestructuring.js
@@ -0,0 +1,30 @@
+//exercicio
+
+//criar uma desestruturação para um objeto chamado "filmes", trazer somente, 3 propriedades
+
+const filmes = [
+  {
+    nomeFilme: "Javascript o Filme",
+    anoLancamento: new Date(),
+    avaliacao: 3,
+    genero: "Terror",
+    duracao: "2h30",
+  },
+  {
+    nomeFilme: "C# o Filme",
+    anoLancamento: new Date(),
+    avaliacao: 5,
+    genero: "Comedia",
+    duracao: "1h30",
+  },
+];
+
+filmes.forEach((filme) => {
+  const { nomeFilme, duracao, avaliacao } = filme;
+
+  console.log(`
+    ${nomeFilme}
+    Duração: ${duracao} 
+    ${avaliacao} Estrelas
+`);
+});
