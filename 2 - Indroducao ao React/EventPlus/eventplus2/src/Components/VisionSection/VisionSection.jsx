d287daa2801ab102723079b7780b509aedd05ccd
Wender <wendermaiac@gmail.com>
2023-11-08 10:52:00 -0300

    final da aula

--- 2 - Indroducao ao React/EventPlus/eventplus2/src/Components/VisionSection/VisionSection.jsx
+++ 2 - Indroducao ao React/EventPlus/eventplus2/src/Components/VisionSection/VisionSection.jsx
@@ -13,7 +13,7 @@ const VisionSection = () => {
                     >
                 </Title>
                 <p className='vision__text'>
-                    Lorem ipsum dolor sit amet.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum doloribus iste doloremque culpa rerum mollitia neque numquam amet! Non unde magni sed provident quaerat rerum voluptatibus, dicta quisquam recusandae. Maxime, maiores unde. Quisquam, exercitationem a expedita quam, facere quas fugit nihil vitae quod saepe omnis qui quis in quibusdam labore nam ipsam vero provident. Eveniet id facilis maxime provident? Natus suscipit quos totam aliquam debitis, corporis magnam ut inventore ducimus nisi, tenetur odio ipsum. Doloribus ea aspernatur iste non, enim quod quo asperiores nesciunt, quia optio libero delectus, voluptatibus dolore obcaecati minus. Excepturi nisi harum ipsa eius similique. Placeat, nesciunt.
                 </p>
             </div>
         </section>
--- 2 - Indroducao ao React/EventPlus/eventplus2/src/Pages/HomePage/HomePage.jsx
+++ 2 - Indroducao ao React/EventPlus/eventplus2/src/Pages/HomePage/HomePage.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import "./HomePage.css";
-import Title from "../../Components/Title/Title";
 import Banner from "../../Components/Banner/Banner";
 import MainContent from "../../Components/MainContent/MainContent";
 import VisionSection from "../../Components/VisionSection/VisionSection";
-
+import ContactSection from "../../Components/ContactSection/ContactSection";
+import NextEvent from "../../Components/NextEvent/NextEvent";
+import Title from "../../Components/Title/Title";
+import Container from "../../Components/Container/Container";
 
 const HomePage = () => {
   return (
     <div>
-      <Banner></Banner>
+      <Banner />
+      <section className="proximos-eventos">
+        <Container>
+          <Title titleText={"Próximos Eventos"} />
+
+          <div className="events-box">
+            <NextEvent title="teste" description="teste evento" eventDate={"08/11/2023"} />
+            <NextEvent title="teste" description="teste evento" eventDate={"08/11/2023"} />
+            <NextEvent title="teste" description="teste evento" eventDate={"08/11/2023"} />
+            <NextEvent title="teste" description="teste evento" eventDate={"08/11/2023"} />
+          </div>
+        </Container>
+      </section>
       <MainContent>
-        <VisionSection></VisionSection>
+        <VisionSection />
       </MainContent>
-      
-      <Title titleText="HomePage" color="" batataClass="margin-above"/>
-      
+
+      <ContactSection />
     </div>
   );
 };
